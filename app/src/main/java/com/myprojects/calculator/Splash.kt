d3568bb4acbd048e323ac92ad2e4327b172929c0
gtran6 <gtran6@depaul.edu>
2023-05-11 16:27:28 -0500

    splash layout set up

---
+++ app/src/main/java/com/myprojects/calculator/Splash.kt
@@ -0,0 +1,24 @@
+package com.myprojects.calculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.myprojects.calculator.databinding.ActivitySplashBinding
+
+class Splash : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed( {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 4000)
+    }
+}
